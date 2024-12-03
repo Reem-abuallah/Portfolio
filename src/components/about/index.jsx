1b1fafaeda2039b96bed4b6b1fdb238e7ef89f14
Reem <162626870+Reem-abuallah@users.noreply.github.com>
2024-12-03 21:37:07 +0300

    sql server

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -43,7 +43,7 @@ const AboutDetails = () => {
         <ItemLayout className={"col-span-full"}>
           <img
             className="w-full h-auto"
-            src={`https://skillicons.dev/icons?i=vscode,css,html,js,cs,react,nextjs,nodejs,npm,tailwind,threejs,vercel,flutter,dart,github,git,mysql,mysql server,firebase`}
+            src={`https://skillicons.dev/icons?i=vscode,css,html,js,cs,react,nextjs,nodejs,npm,tailwind,threejs,vercel,flutter,dart,github,git,mysql,sql server,firebase`}
             alt="CodeBucks"
             loading="lazy"
           />
