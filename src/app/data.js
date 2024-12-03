35f17aac9995433325ef1d034be3cfa805dff1ad
Reem-abuallah <abuallahreem@gmail.com>
2024-12-03 19:10:52 +0300

    first commit

---
+++ src/app/data.js
@@ -0,0 +1,84 @@
+/*
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Netflix Clone",
+    description: "app using Dart and powered by TMDB API",
+    date: "-2024-01-7",
+    demoLink: "https://ecotracker.example.com",
+  },
+  {
+    id: 2,
+    name: "Tour Travel",
+    description: " Website for booking flight ticket",
+    date: "2024-03-16",
+    demoLink: "https://reem-abuallah.github.io/tour-travel/",
+  },
+  {
+    id: 3,
+    name: "Skincare Products",
+    description: "Website for skincare products",
+    date: "2024-05-16",
+    demoLink: "https://reem-abuallah.github.io/Skincare-products/",
+  },
+  {
+    id: 4,
+    name: "Headphones",
+    description: "Headphones shop",
+    date: "2024-6-5",
+    demoLink: "https://reem-abuallah.github.io/Headphones/",
+  },
+  {
+    id: 5,
+    name: "Food Delivery",
+    description: "food delivery app using Dart",
+    date: "2024-08-12",
+    demoLink: "https://recipefinder.example.com",
+  },
+  {
+    id: 6,
+    name: "Gaming",
+    description: "Gaming website by React.js,Tailwind CSS,GSAP",
+    date: "2024-11-23",
+    demoLink: "https://journeylogger.example.com",
+  },
+
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/Reem-abuallah",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/reem-abuallah-6b2783263/",
+    icon: "linkedin",
+    newTab: true,
+  },
+
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ src/components/about/index.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+          Computer Science
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+            My journey in web development is powered by an array of mystical
+            tools and languages, with JavaScript casting the core of my
+            enchantments. I wield frameworks like React.js and Next.js, crafting seamless portals (websites) that connect realms
+            (users) across the digital universe. while my design skills ensure every creation is not only functional
+            but visually captivating.
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+          Different<sub className="font-semibold text-base"> Projects</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            4+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=vscode,css,html,js,cs,react,nextjs,nodejs,npm,tailwind,threejs,flutter,dart,github,mysql,firebase,git`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+  
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
